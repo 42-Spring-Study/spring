be7f30cbb33741e36bf3842e7636aefcf1b26e86
seongtaekkim <chxortnl@gmail.com>
2024-04-15 13:33:28 +0900

    Feat: ADD 변수SpringEL - Object, Map, List

--- src/seongtki/thymeleaf-grammar/src/main/java/me/staek/BasicController.java
+++ src/seongtki/thymeleaf-grammar/src/main/java/me/staek/BasicController.java
@@ -6,6 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
@@ -21,5 +26,21 @@ public class BasicController {
     public String TextUnescaped(Model model) {
         model.addAttribute("data", "hello <b>thymeleaf</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+        return "basic/variable";
     }
 }
