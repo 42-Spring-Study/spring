8fd7776d56e12da3edecf86aafb2da8262ba0086
seongtaekkim <chxortnl@gmail.com>
2024-04-16 14:16:25 +0900

    Feat: ADD 상품등록 controller, html 작성

---
+++ src/seongtki/item-service/src/main/java/me/staek/itemservice/web/item/basic/BasicItemController.java
@@ -0,0 +1,37 @@
+package me.staek.itemservice.web.item.basic;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import me.staek.itemservice.domain.item.Item;
+import me.staek.itemservice.domain.item.ItemRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository repository;
+
+    /**
+     * 테스트데이터
+     */
+    @PostConstruct
+    public void init() {
+        repository.save(new Item("testA", 10000, 10));
+        repository.save(new Item("testB", 20000, 20));
+    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = repository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+}
