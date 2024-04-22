87c18ac2f86024f8a9a13b1ad6f6691a462ebc3c
soyeong Lee <gaya1273@naver.com>
2024-04-22 21:19:39 +0900

    Add: 로그인 유지하기(쿠키)

--- src/soylee/login/src/main/java/hello/login/web/login/LoginController.java
+++ src/soylee/login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,8 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,7 +28,7 @@ public class LoginController {
     }
 
     @PostMapping
-    public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult result) {
+    public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, HttpServletResponse response) {
         if (result.hasErrors()) {
             return "login/loginForm";
         }
@@ -34,11 +36,24 @@ public class LoginController {
         Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
         log.info("login? {}", loginMember);
 
+        //로그인 실패 시
         if (loginMember == null) {
             //loginFail: 현 시점에 존재하지 않는 Message
             result.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
+
+        //로그인 성공 시
+
+        /**
+         * NOTE: 응답에 로그인 한 사람의 정보(아이디)를 쿠키 형태로 담아주었다.
+         *      브라우저가 닫히기 전까지 쿠키(아이디)를 계속 서버에 전달
+         *      페이지에 로그인 정보, 즉 누가 로그인 했는지 보여줄 수 있다.
+         */
+        //세션쿠키(시간 부여 x 쿠키) 생성
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        //응답에 직접 넣어준다
+        response.addCookie(idCookie);
         return "redirect:/";
     }
 }
