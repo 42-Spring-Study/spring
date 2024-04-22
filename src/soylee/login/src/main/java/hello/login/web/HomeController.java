61f265aea81e3a90873c87f6544f58e1fbad0f4b
soyeong Lee <gaya1273@naver.com>
2024-04-22 21:27:47 +0900

    Add: 로그아웃 구현(쿠키)

--- src/soylee/login/src/main/java/hello/login/web/HomeController.java
+++ src/soylee/login/src/main/java/hello/login/web/HomeController.java
@@ -2,12 +2,15 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -15,13 +18,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     private final MemberRepository memberRepository;
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
     @GetMapping
-    public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model){
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             return "home";
         }
@@ -33,5 +36,22 @@ public class HomeController {
 
         model.addAttribute("member", loginMember);
         return "loginHome";
+    }
+
+    /**
+     * NOTE: logout 구현(쿠키)
+     *      쿠키 종료 날짜 0으로 지정
+     *      세션쿠키이므로 브라우저 종료 시에도 로그아웃 된다.
+     */
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
