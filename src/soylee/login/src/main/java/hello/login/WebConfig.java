df2628ef673e1d9fa309937a8437b25f87738fbe
soyeong Lee <gaya1273@naver.com>
2024-04-23 18:49:33 +0900

    Feat: Log Interceptor 구현 및 적용

--- src/soylee/login/src/main/java/hello/login/WebConfig.java
+++ src/soylee/login/src/main/java/hello/login/WebConfig.java
@@ -2,14 +2,17 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
+public class WebConfig implements WebMvcConfigurer {
+    //@Bean
     // NOTE: FilterRegistrationBean 사용 시, springBoot 가 WAS 띄울 때, filter 알아서 넣어준다
     public FilterRegistrationBean<Filter> logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
@@ -26,5 +29,13 @@ public class WebConfig {
         filterRegistrationBean.setOrder(2);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
     }
 }
