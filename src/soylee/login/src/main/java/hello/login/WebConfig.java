591cd4c0c51f9da3871edb899ad01907795d169c
soyeong Lee <gaya1273@naver.com>
2024-04-23 16:39:40 +0900

    Add: Log filter 생성 및 적용

---
+++ src/soylee/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,20 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    // NOTE: FilterRegistrationBean 사용 시, springBoot 가 WAS 띄울 때, filter 알아서 넣어준다
+    public FilterRegistrationBean<Filter> logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
