e39d4cfd45720de5343d691d2a0ec102bd9cf199
seongtaekkim <chxortnl@gmail.com>
2024-04-24 15:36:12 +0900

    Feat: ADD 서버에서 등록한 예외정보 확인 가능

--- src/seongtki/item-service/src/main/java/me/staek/exception/ErrorPageController.java
+++ src/seongtki/item-service/src/main/java/me/staek/exception/ErrorPageController.java
@@ -10,9 +10,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ErrorPageController {
 
+    // RequestDispatcher 상수로 정의되어 있음
+    /**
+     * 서버에서 등록한 예외정보 확인 가능
+     * jakarta.servlet.error.exception : 예외
+     * jakarta.servlet.error.exception_type : 예외 타입
+     * jakarta.servlet.error.message : 오류 메시지
+     * jakarta.servlet.error.request_uri : 클라이언트 요청 URI
+     * jakarta.servlet.error.servlet_name : 오류가 발생한 서블릿 이름
+     * jakarta.servlet.error.status_code : HTTP 상태 코드
+     */
+    public static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
+    public static final String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
+
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
+        printErrorInfo(request);
         return "error-page/404";
     }
 
@@ -27,6 +45,17 @@ public class ErrorPageController {
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
+        printErrorInfo(request);
         return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: ex=", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); //ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.info("dispatchType={}", request.getDispatcherType());
     }
 }
