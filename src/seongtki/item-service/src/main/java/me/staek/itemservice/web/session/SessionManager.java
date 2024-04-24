53418a25434c842cfc499dbb5cf2d6bb651ae14a
seongtaekkim <chxortnl@gmail.com>
2024-04-24 10:52:29 +0900

    Feat: ADD UUID를 이용한 세션관리 클래스 생성

---
+++ src/seongtki/item-service/src/main/java/me/staek/itemservice/web/session/SessionManager.java
@@ -0,0 +1,51 @@
+package me.staek.itemservice.web.session;
+
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 세션 관리
+ */
+@Component
+public class SessionManager {
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    public void createSession(Object value, HttpServletResponse response) {
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(cookie);
+    }
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie cookie = findCookie(request);
+        if (cookie == null)
+            return null;
+        return sessionStore.get(cookie.getValue());
+    }
+
+    private static Cookie findCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        Cookie cookie = Arrays.stream(cookies)
+                                .filter(f -> f.getName().equals(SESSION_COOKIE_NAME))
+                                .findAny().orElse(null);
+        return cookie;
+    }
+
+    public void expire(HttpServletRequest request) {
+        Cookie cookie = findCookie(request);
+        if (cookie != null)
+            sessionStore.remove(cookie.getValue());
+    }
+}
