84a989a0a41a9613c6817a52fb6252defc274945
seongtaekkim <chxortnl@gmail.com>
2024-05-08 17:18:48 +0900

    Test: ADD 예외회복 가능한 로직처리 테스트

---
+++ src/seongtki/item-service/src/test/java/me/staek/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -0,0 +1,99 @@
+package me.staek.jdbc.exception.translator;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.staek.jdbc.BankAccount;
+import me.staek.jdbc.repository.ex.MyDbException;
+import me.staek.jdbc.repository.ex.MyDuplicateKeyException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static me.staek.jdbc.ConnectionConst.*;
+
+
+/**
+ * repository의 회복가능한 예외처리 테스트코드 작성
+ * - 중복된 account id 처리
+ */
+@Slf4j
+public class ExTranslatorV1Test {
+
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("myId");
+        service.create("myId");// 같은 ID 저장 시도
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class Service {
+        private final Repository repository;
+
+        public void create(String memberId) {
+            try {
+                repository.save(new BankAccount(memberId, 0));
+                log.info("saveId={}", memberId);
+            } catch (MyDuplicateKeyException e) {
+                log.info("키 중복, 복구 시도");
+                String retryId = generateNewId(memberId);
+                log.info("retryId={}", retryId);
+                repository.save(new BankAccount(retryId, 0));
+            } catch (MyDbException e) {
+                log.info("데이터 접근 계층 예외", e);
+                throw e;
+            }
+        }
+
+        private String generateNewId(String memberId) {
+            return memberId + new Random().nextInt(10000);
+        }
+
+    }
+
+    @RequiredArgsConstructor
+    static class Repository {
+        private final DataSource dataSource;
+
+        public BankAccount save(BankAccount account) {
+            String sql = "insert into account(account_id, money) values(?,?)";
+            Connection con = null;
+            PreparedStatement pstmt = null;
+
+            try {
+                con = dataSource.getConnection();
+                pstmt = con.prepareStatement(sql);
+                pstmt.setString(1, account.getAccountId());
+                pstmt.setInt(2, account.getMoney());
+                pstmt.executeUpdate();
+                return account;
+            } catch (SQLException e) {
+                // h2
+                if (e.getErrorCode() == 23505) {
+                    throw new MyDuplicateKeyException(e);
+                }
+                throw new MyDbException(e);
+            } finally {
+                JdbcUtils.closeStatement(pstmt);
+                JdbcUtils.closeConnection(con);
+            }
+        }
+    }
+}
