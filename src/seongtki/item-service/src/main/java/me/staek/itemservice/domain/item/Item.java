913d0a14e684f9ad3f9116573e1ffaa0b3d71825
seongtaekkim <chxortnl@gmail.com>
2024-05-14 18:26:05 +0900

    Feat: MOD jsp Item 객체를 Entity로 적용

--- src/seongtki/item-service/src/main/java/me/staek/itemservice/domain/item/Item.java
+++ src/seongtki/item-service/src/main/java/me/staek/itemservice/domain/item/Item.java
@@ -1,6 +1,7 @@
 package me.staek.itemservice.domain.item;
 
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,39 +10,33 @@ import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
+/**
+ * JPA, Spring JPA, querydsl
+ */
 @Data
+@Entity
 public class Item {
-//    @NotNull(groups = UpdateCheck.class)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /**
-     * NotBlank.item.itemName
-     * NotBlank.itemName
-     * NotBlank.java.lang.String
-     * NotBlank
-     */
-//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "필수값입니다.")
+    @Column(name = "item_name", length=10)
     private String itemName;
-    /**
-     * Range.item.price
-     * Range.price
-     * Range.java.lang.Integer
-     * Range
-     */
-//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-//    @Range(min = 1000, max = 100000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-//    @Max(value = 9999, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer quantity;
 
     /**
      * 추가
      * MVC2 타임리프 스프링통합
      */
+    @Transient
     private boolean open; // 상품판여부
+
+    @Transient
     private List<String> regions; // 등록지역
+    @Transient
     private ItemType itemType; // 상품종류
+    @Transient
     private String deliveryCode; // 배송방식
 
     public Item(){}
@@ -52,3 +47,50 @@ public class Item {
     }
 
 }
+
+/**
+ * JDBC, JDBCTemplate, Mybatis
+ */
+//@Data
+//public class Item {
+//    //    @NotNull(groups = UpdateCheck.class)
+//    private Long id;
+//
+//    /**
+//     * NotBlank.item.itemName
+//     * NotBlank.itemName
+//     * NotBlank.java.lang.String
+//     * NotBlank
+//     */
+////    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "필수값입니다.")
+//    private String itemName;
+//    /**
+//     * Range.item.price
+//     * Range.price
+//     * Range.java.lang.Integer
+//     * Range
+//     */
+////    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+////    @Range(min = 1000, max = 100000, groups = {SaveCheck.class, UpdateCheck.class})
+//    private Integer price;
+//    //    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+////    @Max(value = 9999, groups = {SaveCheck.class, UpdateCheck.class})
+//    private Integer quantity;
+//
+//    /**
+//     * 추가
+//     * MVC2 타임리프 스프링통합
+//     */
+//    private boolean open; // 상품판여부
+//    private List<String> regions; // 등록지역
+//    private ItemType itemType; // 상품종류
+//    private String deliveryCode; // 배송방식
+//
+//    public Item(){}
+//    public Item(String itemName, Integer price, Integer quantity) {
+//        this.itemName = itemName;
+//        this.price = price;
+//        this.quantity = quantity;
+//    }
+//
+//}
