09d4035f944e254f2bd429b63e4fafb9f17d8cf0
seongtaekkim <chxortnl@gmail.com>
2024-05-14 11:14:01 +0900

    Feat: ADD SimpleJdbcInsert를 이용해서 Insert 로직 개선

---
+++ src/seongtki/item-service/src/main/java/me/staek/itemservice/domain/item/JdbcTemplateItemRepositoryV3.java
@@ -0,0 +1,113 @@
+package me.staek.itemservice.domain.item;
+
+import lombok.extern.slf4j.Slf4j;
+import me.staek.itemservice.web.validation.dto.ItemSearchCond;
+import me.staek.itemservice.web.validation.dto.ItemUpdateDto;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * 데이터 접근 기술
+ * - JDBC & 메모리 디비
+ *
+ * SimpleJdbcInsert - insert 쿼리와 키 자동증가 로직을 도와주는 클래스 적용
+ * 동적쿼리 작성은 여전히 까다롭다.
+ */
+@Slf4j
+@Repository
+public class JdbcTemplateItemRepositoryV3 implements ItemRepository {
+
+    private final NamedParameterJdbcTemplate template;
+    private final SimpleJdbcInsert jdbcInsert;
+
+    public JdbcTemplateItemRepositoryV3(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("item")
+                .usingGeneratedKeyColumns("id");
+//                .usingColumns("item_name", "price", "quantity");
+    }
+
+
+    @Override
+    public Item save(Item item) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(item);
+        Number key = jdbcInsert.executeAndReturnKey(param);
+        item.setId(key.longValue());
+        return item;
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id, item_name, price, quantity from item where id = :id";
+        try {
+            Map<String, Object> param = Map.of("id", id);
+            Item item = template.queryForObject(sql, param, itemRowMapper());
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Item> findAll() {
+        String sql = "select id, item_name, price, quantity from item";
+        return template.query(sql, itemRowMapper());
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        Integer maxPrice = cond.getMaxPrice();
+        String itemName = cond.getItemName();
+        SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+        String sql = "select id, item_name, price, quantity from item";
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= :maxPrice";
+        }
+        log.info("sql={}", sql);
+        return template.query(sql, param, itemRowMapper());
+    }
+
+    @Override
+    public void update(Long id, ItemUpdateDto uptItem) {
+        String sql = "update item " +
+                "set item_name=:itemName, price=:price, quantity=:quantity " +
+                "where id=:id";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("itemName", uptItem.getItemName())
+                .addValue("price", uptItem.getPrice())
+                .addValue("quantity", uptItem.getQuantity())
+                .addValue("id", id);
+        template.update(sql, param);
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return BeanPropertyRowMapper.newInstance(Item.class);
+    }
+}
