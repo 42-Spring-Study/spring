4a90918217ece7e6d77e4082812f7efd9ffa0dbc
soyeong Lee <gaya1273@naver.com>
2024-05-22 17:03:25 +0900

    feat: propagation: outer rollback

--- src/soylee/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/soylee/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -98,7 +98,24 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋 시작");
         txManager.commit(outer);
         log.info("외부 트랜잭션 커밋 완료");
+    }
 
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
 
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋 시작");
+        txManager.commit(inner);
+        log.info("내부 트랜잭션 커밋 완료");
+
+        log.info("외부 트랜잭션 롤백 시작");
+        txManager.rollback(outer);
+        log.info("외부 트랜잭션 롤백 완료");
     }
 }
