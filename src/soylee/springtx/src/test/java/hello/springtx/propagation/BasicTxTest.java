975d0a5779334a56ff2acaeab4f75a0985d7813a
soyeong Lee <gaya1273@naver.com>
2024-05-22 17:01:16 +0900

    feat: transaction propagation

--- src/soylee/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/soylee/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -80,4 +80,25 @@ public class BasicTxTest {
         txManager.rollback(tx2);
         log.info("트랜잭션2 롤백 완료");
     }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋 시작");
+        txManager.commit(inner);
+        log.info("내부 트랜잭션 커밋 완료");
+
+        log.info("외부 트랜잭션 커밋 시작");
+        txManager.commit(outer);
+        log.info("외부 트랜잭션 커밋 완료");
+
+
+    }
 }
