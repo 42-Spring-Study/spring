d23aea2a8e4b4eaca73307054bdf790d0699f2c5
seongtaekkim <chxortnl@gmail.com>
2024-05-29 13:43:16 +0900

    Feat: ADD Order 와 1:1 연관관계 Entity 생성

---
+++ src/seongtki/jpa-practice/src/main/java/me/staek/shop/domain/Delivery.java
@@ -0,0 +1,20 @@
+package me.staek.shop.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    private String zipcode;
+    private String street;
+    private String city;
+    private DeliveryStatus deliveryStatus;
+}
