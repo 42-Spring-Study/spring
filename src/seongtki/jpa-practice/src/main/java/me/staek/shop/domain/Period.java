a2b316055db3bc5cd814848a5ed01408a65e4115
seongtaekkim <chxortnl@gmail.com>
2024-06-03 17:13:22 +0900

    Feat: ADD Embedded Type

---
+++ src/seongtki/jpa-practice/src/main/java/me/staek/shop/domain/Period.java
@@ -0,0 +1,35 @@
+package me.staek.shop.domain;
+
+
+import jakarta.persistence.Embeddable;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Embeddable
+public class Period {
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Period period = (Period) object;
+        return Objects.equals(getStartDateTime(), period.getStartDateTime()) && Objects.equals(getEndDateTime(), period.getEndDateTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStartDateTime(), getEndDateTime());
+    }
+}
