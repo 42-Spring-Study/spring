6626f45659d4e72fa18f65252255f8620d1a406d
seongtaekkim <chxortnl@gmail.com>
2024-06-05 17:23:41 +0900

    Feat: ADD home html template

--- src/seongtki/jpa-with-springweb/src/main/java/me/staek/shop/App.java
+++ src/seongtki/jpa-with-springweb/src/main/java/me/staek/shop/App.java
@@ -1,4 +1,4 @@
-package me.staek.jpawithspringweb;
+package me.staek.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
